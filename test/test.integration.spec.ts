ad4447d7104fad4e268d6b2ace3cd7b4d1a82076
Diego Lima <diegogoncalvesreisdelima21@gmail.com>
2025-01-29 10:06:34 -0300

    build: configure tsconfig.json to generate build in dist folder

--- test/test.integration.spec.ts
+++ test/test.integration.spec.ts
@@ -1,8 +1,6 @@
 import { app } from "../src/app";
 import request from "supertest";
 
-
-
 describe("GET /", () => {
   it("should return Hello, Express.JS", async () => {
     const res = await request(app).get("/");
